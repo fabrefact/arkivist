3810535f7602c2f58d18cb82f45cdc3bd71b03a6
Sara Farquharson <51892187+sfarqu@users.noreply.github.com>
2023-07-16 02:33:54 -0700

    Clean up routing to allow dependency injection of data store

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/tus/tusd/pkg/filestore"
@@ -11,18 +10,87 @@ import (
 	"os"
 )
 
-// fetch file from s3
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hi"))
+// writeResponse abstracts away writing response bodies entirely because I don't like the syntax
+func writeResponse(w http.ResponseWriter, body string) {
+	_, err := w.Write([]byte(body))
+	if err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
 }
 
-// upload a file to s3
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
+// upload a file to storage
+func uploadMediaHandler(store tusd.DataStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-	// tusd only seems to work for single file uploads, only handle one file at a time
+		// tusd only seems to work for single file uploads, only handle one file at a time
+
+		// A storage backend for tusd may consist of multiple different parts which
+		// handle upload creation, locking, termination and so on. The composer is a
+		// place where all those separated pieces are joined together. In this example
+		// we only use the file store but you may plug in multiple.
+		composer := tusd.NewStoreComposer()
+		composer.UseCore(store)
+
+		// Create a new HTTP handler for the tusd server by providing a configuration.
+		// The StoreComposer property must be set to allow the handler to function.
+		handler, err := tusd.NewUnroutedHandler(tusd.Config{
+			BasePath:              "/media/",
+			StoreComposer:         composer,
+			NotifyCompleteUploads: true,
+		})
+		if err != nil {
+			log.Fatalf("Unable to create handler: %s", err.Error())
+		}
+
+		// need to look into what this does and what is written to the response
+		handler.PostFile(w, r)
+
+		// probably don't need to return a response body at all but for now it's useful
+		writeResponse(w, "Upload successful")
+	}
+}
+
+// retrieve a media file from storage
+func getMediaFileHandler(store tusd.DataStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		writeResponse(w, "hi")
+	}
+}
+
+// delete a media file from storage
+func deleteMediaFileHandler(store tusd.DataStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		writeResponse(w, "hi")
+	}
+}
+
+// createRouter defines all the routes and middleware for the server
+// storage routes use the provided data store
+func createRouter(store tusd.DataStore) http.Handler {
+	r := chi.NewRouter()
+
+	// look into other useful middleware
+	r.Use(middleware.Logger)
+
+	// handle media routes
+	r.Route("/media", func(r chi.Router) {
+		r.Post("/", uploadMediaHandler(store))
+		r.Route("/{mediaId}", func(r chi.Router) {
+			r.Get("/", getMediaFileHandler(store))
+			r.Delete("/", deleteMediaFileHandler(store))
+		})
+	})
+
+	return r
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
 
 	// Need to figure out what configurations are required for S3store
-	// probably shouldn't be created here?
 	// store := s3store.S3Store{}
 
 	// to allow testing for now, lets just save to local directory
@@ -34,53 +102,9 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		Path: "./uploads",
 	}
 
-	// A storage backend for tusd may consist of multiple different parts which
-	// handle upload creation, locking, termination and so on. The composer is a
-	// place where all those separated pieces are joined together. In this example
-	// we only use the file store but you may plug in multiple.
-	composer := tusd.NewStoreComposer()
-	store.UseIn(composer)
+	router := createRouter(store)
 
-	// Create a new HTTP handler for the tusd server by providing a configuration.
-	// The StoreComposer property must be set to allow the handler to function.
-	handler, err := tusd.NewUnroutedHandler(tusd.Config{
-		BasePath:              "/media/",
-		StoreComposer:         composer,
-		NotifyCompleteUploads: true,
-	})
-	if err != nil {
-		log.Fatalf("Unable to create handler: %s", err.Error())
-	}
-
-	// need to look into what this does and what is written to the response
-	handler.PostFile(w, r)
-
-	// Chi has a terrible return ergonomics, write a helper function for this
-	w.Write([]byte(fmt.Sprintf("Upload successful")))
-}
-
-func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-	}
-
-	r := chi.NewRouter()
-
-	// look into other useful middleware
-	r.Use(middleware.Logger)
-
-	// who even knows what this is for
-	r.Get("/", indexHandler)
-
-	// handle media routes
-	r.Route("/media", func(r chi.Router) {
-		r.Post("/", uploadHandler)
-		r.Route("/{mediaId}", func(r chi.Router) {
-			// add get and delete for individual media
-		})
-	})
-	if err := http.ListenAndServe(":"+port, r); err != nil {
+	if err := http.ListenAndServe(":"+port, router); err != nil {
 		log.Fatalf("Unexpected error: %s", err.Error())
 	}
 }
